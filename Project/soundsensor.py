026ff3a42a63001affdb6797d4e731a536021999
bradenhayes <70035017+bradenhayes@users.noreply.github.com>
2020-11-16 15:42:27 -0500

    Update soundsensor.py

--- Project/soundsensor.py
+++ Project/soundsensor.py
@@ -14,7 +14,7 @@ def soundsensed():
     if GPIO.input(17)==GPIO.LOW:
             
             print ("Sound Detected!")
-            params = urllib.parse.urlencode({'field1': 1, 'key':key })
+            params = urllib.parse.urlencode({'field1': Sound has been sensed, 'key':key })
             headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
             conn = http.client.HTTPConnection("api.thingspeak.com:80")
             try:
