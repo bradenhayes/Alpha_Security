2fb50891852847c584debd6702a6e22f52e2907b
bradenhayes <70035017+bradenhayes@users.noreply.github.com>
2020-11-16 15:43:05 -0500

    Update uploadtodropbox.py

--- Project/uploadtodropbox.py
+++ Project/uploadtodropbox.py
@@ -1,7 +1,10 @@
 import dropbox
-
-dbx = dropbox.Dropbox('8WNB4VteJFUAAAAAAAAAAQkkCfn_aeDYNxNuE2p8sIRNh5fWyWuZhLSqwXT5UZ2p')
-dbx.users_get_current_account()
-
-f = open('lol.mp3','rb')
-dbx.files_upload(bytes(f.read()), '/Audio')
+dbx = dropbox.Dropbox('dLbnezFTykIAAAAAAAAAAbWNwp4-LRm3T00dhSgCTwjexlC0jIiHwIp6GeBLz7r4')
+        # Check that the access token is valid
+try:
+            dbx.users_get_current_account()
+except AuthError as err:
+            sys.exit("ERROR: Invalid access token; try re-generating an access token from the app console on the web.")
+with open("/home/pi/Desktop/Sysc3010/Project/Project1/file.mp3", "wb") as f:
+            metadata, res = dbx.files_download(path="/Audio/file.mp3")
+            f.write(res.content)
