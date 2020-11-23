6574091c1caab102b53264a94ec1b4e4de583fa2
James Runtas <71133703+jamesruntas@users.noreply.github.com>
2020-11-22 19:16:16 -0500

    Update motion.py

--- Project/motion.py
+++ Project/motion.py
@@ -2,28 +2,34 @@ import RPi.GPIO as GPIO
 import time
 import http.client
 import urllib.parse
+from datetime import datetime
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
 GPIO.setup(3, GPIO.OUT)         #LED output pin
-key = "EOOMM9Z3VB6QOGAT"  # Put your API read Key here
-count = 0
+key = "EOOMM9Z3VB6QOGAT"        #Put your API read Key here
+location = None
 
 def motionstatus():
     i=GPIO.input(11)
+    if location==None:
+        print ("WARNING: Location Data not available")
+
     assert(i>=0 & i <=1), "Hardware Error"
     if i==0:                 #When output from motion sensor is LOW
         GPIO.output(3, 0)    #Turn OFF LED
         state = "safe"
     elif i==1:               #When output from motion sensor is HIGH
-        print ("Intruder detected",i)
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        print ("Intruder detected:", current_time)
+        print ("Location: ", location)
         GPIO.output(3, 1)  #Turn ON LED
 
-        count = count + 1
-        assert (count > 9999), "Motion Sensor obstructed"
+
         assert(i>=0 & i <=1), "Hardware Error"
         state = "Intruder Detected"
-        params = urllib.parse.urlencode({'field1': i,'key':key })
+        params = urllib.parse.urlencode({'field1': "Motion Detected",'key':key })
         headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
         conn = http.client.HTTPConnection("api.thingspeak.com:80")
         try:
@@ -33,7 +39,15 @@ def motionstatus():
             conn.close()
         except:
             print("connection failed")
+        time.sleep(3)
 
 if __name__ == "__main__":
     while True:
-        motionstatus()
+        print ("Press M to move motion sensor")
+        print ("Press E to enable motion sensor")
+        movekey = input();
+        if movekey =="M":
+            location = input("What is the location?")
+        elif movekey == "E":
+            while True:
+                motionstatus()
